2a8925839180631b83365538701f67d9ec2ae1fa
jmollaf <jmollaf@gmail.com>
2015-11-10 09:29:32 +0100

    PRIMERA VERSION REFRESH

--- PArticulo/PArticulo/MainWindow.cs
+++ PArticulo/PArticulo/MainWindow.cs
@@ -10,20 +10,31 @@ public partial class MainWindow: Gtk.Window
 	{
 		Build ();
 		Console.WriteLine ("MainWindow ctor.");
-		QueryResult queryResult = PersisterHelper.Get ("select * from articulo");
-		TreeViewHelper.Fill (treeView, queryResult);
+		fillTreeView ();
 
 		newAction.Activated += delegate {
 			new ArticuloView();
 		};
 		//newAction.Activated += newActionActivated;
+		refreshAction.Activated += delegate(object sender, EventArgs e) {
+
+			fillTreeView();
+	    };
+	
 	}
 
 //	void newActionActivated (object sender, EventArgs e)
 //	{
 //		new ArticuloView ();
 //	}
-
+	private void fillTreeView(){
+		//Primero tenemos que borrar las columnas existentes en el treeView porque sino se repiten
+		TreeViewColumn[] treeViewColumns = treeView.Columns;
+		foreach (TreeViewColumn treeViewColumn in treeViewColumns)
+			treeView.RemoveColumn (treeViewColumn);
+		QueryResult queryResult = PersisterHelper.Get ("select * from articulo");
+		TreeViewHelper.Fill (treeView, queryResult);
+	}
 	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
 	{
 		Application.Quit ();
--- PArticulo/PArticulo/Program.cs
+++ PArticulo/PArticulo/Program.cs
@@ -1,7 +1,6 @@
 using System;
 using Gtk;
 using MySql.Data.MySqlClient;
-
 using SerpisAd;
 
 namespace PArticulo
